1595c63eeef2e58297ae97ff88809fecc4f23460
daweth <46637826+daweth@users.noreply.github.com>
2021-10-27 14:32:45 -0400

    hello world and introduction to cargo

---
+++ projects/hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/mv/Documents/code/learning-rust/projects/hello-cargo/target/debug/hello-cargo: /home/mv/Documents/code/learning-rust/projects/hello-cargo/src/main.rs
---
+++ projects/hello-cargo/target/release/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/mv/Documents/code/learning-rust/projects/hello-cargo/target/release/hello-cargo: /home/mv/Documents/code/learning-rust/projects/hello-cargo/src/main.rs
